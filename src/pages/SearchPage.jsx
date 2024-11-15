1807d9b5855e04f186b3fffc42bcfe51afeed649
gimjhwan <dds02492@naver.com>
2024-11-15 22:16:46 +0900

    feat: Add search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -2,18 +2,51 @@ import { useState } from "react";
 import styled from "styled-components";
 
 export const SearchPage = () => {
-  const [keyword, setKeyword] = useState;
+  const [keyword, setKeyword] = useState("");
   const onChange = (event) => {
     setKeyword(event.target.value);
   };
 
   return (
-    <Container>
-      <input type="text" value={keyword} onChange={onChange} />
-    </Container>
+    <>
+      <Container>
+        <Header>Search</Header>
+      </Container>
+      <AppWrapper>
+        <SearchBar type="text" value={keyword} onChange={onChange} placeholder="Keywords," />
+      </AppWrapper>
+    </>
   );
 };
 
 const Container = styled.div`
-  // CSS
+  padding: 9px 14px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.header`
+  ${(props) => props.theme.typography.title};
+  color: ${(props) => props.theme.color.black};
+`;
+
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const SearchBar = styled.input`
+  width: 335px;
+  height: 44px;
+  padding: 0 20px;
+  border: 1px solid #d7d7d7;
+  border-radius: 14px;
+  outline: none;
+  font-size: 12px;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-color: #007bff;
+  }
 `;
