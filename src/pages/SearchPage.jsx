7737955a4d90d2af2fc2919ac99d03b378ff7fcc
gimjhwan <dds02492@naver.com>
2024-11-15 22:04:10 +0900

    feat: Add search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export const SearchPage = () => {
+  const [keyword, setKeyword] = useState;
+  const onChange = (event) => {
+    setKeyword(event.target.value);
+  };
+
   return (
     <Container>
-      <h1>Search Page</h1>
+      <input type="text" value={keyword} onChange={onChange} />
     </Container>
   );
 };
